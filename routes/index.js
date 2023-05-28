ba90552c115e85e54411b72d323e6a920c9b1b21
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-05-28 11:04:52 +0700

    Feat : Products and Tags Feature

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const tagRoutes = require('../routes/tag');
 const router = express.Router();
 const productRoutes = require('../routes/product');
-
+const userRoutes = require('../routes/users');
 
 router.use('/tag', tagRoutes);
 router.use('/product', productRoutes);
+router.use('/users', userRoutes);
+
 module.exports = router;
