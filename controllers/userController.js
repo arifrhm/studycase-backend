8e43be5ff0caa8a0769a6f4a2a4718e6801938a8
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-06-11 16:17:08 +0700

    Fix : Separate login function from userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,9 @@
-const { NOT_AUTHENTICATED_CODE,GENERAL_ERROR_CODE } = require("../constant/errorCode");
-const { UNAUTHORIZED, ERROR_SERVER } = require("../constant/errorHttp");
-const { UN_AUTHENTICATED, GENERAL_ERROR_MESSAGE } = require("../constant/errorMessage");
+const { GENERAL_ERROR_CODE } = require("../constant/errorCode");
+const {  ERROR_SERVER } = require("../constant/errorHttp");
+const { GENERAL_ERROR_MESSAGE } = require("../constant/errorMessage");
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const HttpError = require('../interface/httpError');
-const generateToken = require('../utils/generateToken');
 
 const all = async (req, res, next) => {
     try {
@@ -84,43 +83,10 @@ const deleteByID = async (req, res, next) => {
     }
 }
 
-//Create genreate token endpoint
-const login = async function (req, res, next) {
-    try {
-        const { name,
-            email,
-            password,
-            isAdmin,
-        } = req.body;
-        
-        // Check if user exists and password is correct
-        const user = await User.findOne({name: name, email: email });  
-        const validPassword = await bcrypt.compare(password, user.password);
-
-        const errorUnauthenticated = new HttpError(UN_AUTHENTICATED, NOT_AUTHENTICATED_CODE, UNAUTHORIZED);
-
-        if (!user||!validPassword) {
-            next(errorUnauthenticated);
-        }
-        const token = generateToken(user);
-        delete user.password;
-        req.data = {
-            user_email : user.email,
-            token : token
-        };
-        next();
-    } catch (err) {
-        console.log(err);
-        const error = new HttpError(GENERAL_ERROR_MESSAGE, GENERAL_ERROR_CODE, ERROR_SERVER);
-        next(error);
-    }
-  }
-
 module.exports = {
     create,
     all,
     getbyID,
     updateByID,
-    deleteByID,
-    login
+    deleteByID
 }
