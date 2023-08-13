bf64a7971c3eb107fa96af031b667df8e353ab25
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-08-13 18:37:30 +0700

    Fix : Unique email for register

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email : { type: String, required: true },
+    email : { type: String, required: true, unique: true // `email` must be unique 
+    },
     password: { type: String, required: true },
 });
 
